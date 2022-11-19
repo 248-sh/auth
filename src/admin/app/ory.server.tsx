a0e8990c0cc8f22d6bc1090b5137b016e9231560
miran248 <miran@248.sh>
2022-11-19 17:19:14 +0100

    chore: use env vars

--- src/admin/app/ory.server.tsx
+++ src/admin/app/ory.server.tsx
@@ -3,8 +3,8 @@ import { V0alpha2Api } from "@ory/kratos-client";
 // import { ApiApi } from "@ory/oathkeeper-client";
 // import { JwkApi, OidcApi, OAuth2Api } from "@ory/hydra-client";
 
-export const keto = new ReadApi(undefined, "http://localhost:4466");
-export const kratos = new V0alpha2Api(undefined, "http://localhost:4434");
+export const keto = new ReadApi(undefined, process.env.KETO_READ_ENDPOINT);
+export const kratos = new V0alpha2Api(undefined, process.env.KRATOS_ADMIN_ENDPOINT);
 // export const oathkeeper = new ApiApi(undefined, "http://localhost:4456");
 
 // new JwkApi().
