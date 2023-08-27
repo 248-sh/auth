6a0935d893bd542f4429453be65a06307e990660
miran248 <miran@248.sh>
2023-08-27 20:05:57 +0200

    chore: use public kratos url with redirects

--- src/website/app/root.tsx
+++ src/website/app/root.tsx
@@ -40,6 +40,10 @@ const App = () => (
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/manifest.webmanifest" />
       <Meta />
       <Links />
     </head>
